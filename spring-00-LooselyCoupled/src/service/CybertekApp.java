3ec797ad9b7e9e1b563aeb02596dfa6485c9d277
Oyun Tsolmon <oyun9121@gmail.com>
2020-10-17 10:30:03 -0600

    2nd commit

--- spring-00-LooselyCoupled/src/service/CybertekApp.java
+++ spring-00-LooselyCoupled/src/service/CybertekApp.java
@@ -4,10 +4,10 @@ import implementation.Mentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
-//        FullTimeMentor fullTime = new FullTimeMentor();
-        PartTimeMentor partTime = new PartTimeMentor();//new keyword always create dependency
-
-        MentorAccount mentor = new MentorAccount(partTime);
-        mentor.manageAccount();
+////        FullTimeMentor fullTime = new FullTimeMentor();
+//        PartTimeMentor partTime = new PartTimeMentor();//new keyword always create dependency
+//
+//        MentorAccount mentor = new MentorAccount(partTime);
+//        mentor.manageAccount();
     }
 }
