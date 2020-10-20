3d4594fa1090af9643718b41ac090ebd586ca83a
tsolmonOT <otsol2005@gmail.com>
2020-10-20 17:47:56 -0600

    Constructor added to Java class

--- Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
+++ Spring-02-DI-ConstructorInjection/src/main/java/com/cybertek/services/Java.java
@@ -5,6 +5,14 @@ import com.cybertek.interfaces.Course;
 public class Java implements Course {
     OfficeHours officeHours;
 
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+    //Java class is dependent on officeHours class
+    //How we inject office hour obj into Java obj ?
+    //Thru Constructor injection, Setter injection, Field injection
+
+
     public void getTeachingHours() {
         System.out.println("Weekly teaching hours : " + (20 + officeHours.getHours()));
     }
