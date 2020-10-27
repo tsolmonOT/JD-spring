8dfe86e0f7b614857802c766dbbb9b01271df2e4
tsolmonOT <otsol2005@gmail.com>
2020-10-27 08:25:21 -0500

    Autowiring

---
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,36 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import jdk.nashorn.internal.objects.annotations.Setter;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Java implements Course {
+
+    @Autowired
+    private OfficeHours officeHours;
+
+    //Constructor injection => if you have only 1 constructor @Autowired is optional
+//    @Autowired
+//    public Java(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+    //Setter injection
+    //Annotation is required bc there is multiple setters
+//    @Autowired
+//    public void setOfficeHours(OfficeHours officeHours) {
+//        this.officeHours = officeHours;
+//    }
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours : " +(30+officeHours.getHours()));
+    }
+}
