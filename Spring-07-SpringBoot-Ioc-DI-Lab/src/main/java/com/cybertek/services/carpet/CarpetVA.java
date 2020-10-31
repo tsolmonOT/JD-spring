a5000ba9eb92e19e93df30e89301190f78e7e5a3
tsolmonOT <otsol2005@gmail.com>
2020-10-31 13:50:38 -0500

    Carpet package created

---
+++ Spring-07-SpringBoot-Ioc-DI-Lab/src/main/java/com/cybertek/services/carpet/CarpetVA.java
@@ -0,0 +1,30 @@
+package com.cybertek.services.carpet;
+
+import com.cybertek.enums.City;
+import com.cybertek.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+public class CarpetVA implements Carpet {
+    private static final Map<City, BigDecimal> sqPriceForCity = new HashMap<>();
+
+    static {
+        sqPriceForCity.put(City.MCLEAN, new BigDecimal("4.32"));
+        sqPriceForCity.put(City.ARLINGTON, new BigDecimal("2.92"));
+        sqPriceForCity.put(City.FAIRFAX, new BigDecimal("4.65"));
+    }
+
+    @Override
+    public BigDecimal getSqFtPrice(City city) {
+        BigDecimal defaultValue = BigDecimal.ZERO;
+        //entrySet() converts map to set  //findFirst will return Optional //if it's not Optional it would return null pointer exception
+        Optional<Map.Entry<City, BigDecimal>> collect = sqPriceForCity.entrySet().stream().filter(x -> x.getKey() == city).findFirst();
+        //if city didn't match return 0
+        return collect.isPresent() ? collect.get().getValue() : defaultValue;
+    }
+}
